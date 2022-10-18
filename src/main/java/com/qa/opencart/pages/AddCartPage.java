546216549f6b1aaa877fea06e61cc72c60bcea84
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-10-18 09:03:53 +0530

    updated code on the basis of naveen's review

--- src/main/java/com/qa/opencart/pages/AddCartPage.java
+++ src/main/java/com/qa/opencart/pages/AddCartPage.java
@@ -1,10 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class AddCartPage {
-	
-	
+
 	String name = "iMac";
-	
-	
+
+	By loc = By.id("product");
+
+	public void click() {
+		System.out.println("click --" + loc);
+	}
 
 }
+
